c894c884c4382f436149796fcb13cf2327f6b55c
UnivDonguk22 <kampo5096@hanyang.ac.kr>
2022-10-27 03:19:05 +0900

    :fire: Start NestJS Studing - Module & Controller & Service
    10/26일 NestJS 공부를 본격적으로 시작했다. 오늘은 모듈, 컨트롤러, 서비스를 구성하는 방법에 대해 배웠다.

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+
+@Controller('boards')
+export class BoardsController{
+    constructor(private boardsService: BoardsService){
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
+  // AppModule에 대한 Module을 생성함.
+  // 이 Module 자체가 라우팅 파일이 되는 듯~
+  
+  // Root Module을 생성해줌.
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
